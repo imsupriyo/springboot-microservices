bb5e0678fe0eacaf4d77332bb516c27fb5d455a7
supriyo <supriyopal388@gmail.com>
2024-11-14 01:48:32 +0530

    Add Circuit Breaker functionality with Resilience4j

--- employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/example/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -8,13 +8,17 @@ import com.example.employeeservice.exception.ResourceNotFoundException;
 import com.example.employeeservice.repository.EmployeeRepository;
 import com.example.employeeservice.service.APIClient;
 import com.example.employeeservice.service.EmployeeService;
+import io.github.resilience4j.retry.annotation.Retry;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(EmployeeServiceImpl.class);
     private EmployeeRepository employeeRepository;
     private ModelMapper modelMapper;
     //    private RestTemplate restTemplate;
@@ -30,8 +34,12 @@ public class EmployeeServiceImpl implements EmployeeService {
         return modelMapper.map(savedEmployee, EmployeeDTO.class);
     }
 
+    //    @CircuitBreaker(name = "${spring.application.name}", fallbackMethod = "getDefaultEmployee")
+    @Retry(name = "${spring.application.name}", fallbackMethod = "getDefaultEmployee")
     @Override
     public APIResponseDTO getEmployeeById(Long employeeId) {
+        LOGGER.info("inside getEmployeeById() method");
+
         Employee employee = employeeRepository.findById(employeeId)
                 .orElseThrow(() -> new ResourceNotFoundException("employee", "id", employeeId));
 
@@ -45,6 +53,26 @@ public class EmployeeServiceImpl implements EmployeeService {
 //                .block();
 
         DepartmentDTO departmentDTO = apiClient.getDepartment(employee.getDepartmentCode());
+
+        return APIResponseDTO.builder()
+                .employee(modelMapper.map(employee, EmployeeDTO.class))
+                .department(departmentDTO)
+                .build();
+    }
+
+    public APIResponseDTO getDefaultEmployee(Long employeeId, Exception e) {
+        LOGGER.info("inside getDefaultEmployee() method");
+
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("employee", "id", employeeId));
+
+        DepartmentDTO departmentDTO = new DepartmentDTO
+                (
+                        2L,
+                        "R&D",
+                        "Research and Development",
+                        "RD101"
+                );
 
         return APIResponseDTO.builder()
                 .employee(modelMapper.map(employee, EmployeeDTO.class))
